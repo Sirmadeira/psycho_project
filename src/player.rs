d6706fdbac4a2dad38ca37a7c2a78a5dceca8606
Sirmadeira <nicolaumori@gmail.com>
2024-01-08 12:46:46 -0300

    VOltando

--- src/player.rs
+++ src/player.rs
@@ -6,8 +6,8 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
-            .add_systems(Update, player_jump_dash)
-            .add_systems(Update, player_movement);
+            .add_systems(Update, player_movement)
+            .add_systems(Update, player_jump_dash);
     }
 }
 
@@ -24,17 +24,17 @@ fn spawn_player(mut commands: Commands, assets: Res<AssetServer>) {
     let player = (
         SceneBundle {
             scene: assets.load("beta.glb#Scene0"),
-            transform: Transform::from_xyz(2.0, 0.0, 0.0),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..default()
         },
         Player,
-        Speed { walk: 5.0 },
+        Speed { walk: 3.0 },
         ThirdPersonCameraTarget,
         Name::new("Player"),
     );
-
     commands
-        .spawn(RigidBody::KinematicPositionBased)
+        .spawn(RigidBody::Dynamic)
+        .insert(player)
         .with_children(|children| {
             children
                 .spawn(Collider::cuboid(0.2, 0.5, 0.2))
@@ -44,7 +44,6 @@ fn spawn_player(mut commands: Commands, assets: Res<AssetServer>) {
                 .insert(Ccd::enabled())
                 .insert(GravityScale(1.0));
         })
-        .insert(player)
         .insert(Velocity {
             linvel: Vec3::new(0.0, 0.0, 0.0),
             angvel: Vec3::new(0.0, 0.0, 0.0),
@@ -54,16 +53,16 @@ fn spawn_player(mut commands: Commands, assets: Res<AssetServer>) {
 
 fn player_movement(
     keys: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut player_q: Query<(&mut Transform, &Speed), With<Player>>,
+    mut player_q: Query<(&mut Velocity, &mut Transform, &Speed), With<Player>>,
     cam_q: Query<&Transform, (With<Camera3d>, Without<Player>)>,
 ) {
-    for (mut player_transform, player_speed) in player_q.iter_mut() {
+    for (mut player_velocity, mut player_transform, player_speed) in player_q.iter_mut() {
         let cam = match cam_q.get_single() {
             Ok(c) => c,
             Err(e) => Err(format!("Erro pegando o objeto de camera: {}", e)).unwrap(),
         };
         let mut direction: Vec3 = Vec3::ZERO;
+
         if keys.pressed(KeyCode::W) {
             direction = cam.forward();
         }
@@ -88,11 +87,10 @@ fn player_movement(
         if keys.pressed(KeyCode::S) & keys.pressed(KeyCode::D) {
             direction = (cam.back() + cam.right()) / 2.0;
         }
-
         direction.y = 0.0;
-
-        player_transform.translation += direction * player_speed.walk * time.delta_seconds();
-
+        player_velocity.linvel.x = direction.x * player_speed.walk;
+        player_velocity.linvel.z = direction.z * player_speed.walk;
+        player_velocity.linvel.y += direction.y;
         if direction.length_squared() > 0.0 {
             player_transform.look_to(cam.forward(), Vec3::Y)
         }
@@ -102,10 +100,21 @@ fn player_movement(
 fn player_jump_dash(
     keys: Res<Input<KeyCode>>,
     mut player_velocity: Query<&mut Velocity, With<Player>>,
+    cam_q: Query<&Transform, (With<Camera3d>, Without<Player>)>,
 ) {
     for mut vel in player_velocity.iter_mut() {
+        let cam = match cam_q.get_single() {
+            Ok(c) => c,
+            Err(e) => Err(format!("Erro pegando o objeto de camera: {}", e)).unwrap(),
+        };
+        let mut direction = Vec3::new(0.0, 0.0, 0.0);
+
         if keys.pressed(KeyCode::Space) {
             vel.linvel = Vec3::new(0.0, 10.0, 0.0);
         }
+        if keys.just_pressed(KeyCode::W) & keys.pressed(KeyCode::W) {
+            direction = cam.forward() * 10.0
+        }
+        vel.linvel = direction
     }
 }
