a689710a9c4fbd3d2d9780e1eedcd59347b2bd37
Sirmadeira <nicolaumori@gmail.com>
2024-03-28 18:22:43 +0000

    Simple jump and simple ground contact

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
-use bevy::{ input::keyboard::Key, prelude::*, time::Stopwatch};
+use bevy::{ prelude::*, time::Stopwatch};
 use bevy_rapier3d::prelude::*;
 
 pub struct PlayerPlugin;
@@ -8,17 +8,22 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<MovementAction>();
         app.add_systems(Startup, (spawn_hitbox,spawn_time).chain());
-        app.add_systems(Update,(keyboard_walk,keyboard_dash,move_character).chain());
+        app.add_systems(Update,(display_events,keyboard_walk,keyboard_dash,keyboard_jump,move_character,apply_movement_damping).chain());
     }
 }
+
 
 
 #[derive(Event)]
 pub enum MovementAction {
     Move(Vec2),
+    Dash(Vec2),
     Jump,
 }
 
+#[derive(Component)]
+#[component(storage = "SparseSet")]
+pub struct Grounded;
 
 #[derive(Component)]
 pub struct Player;
@@ -31,27 +36,34 @@ struct Timers{
     up: Stopwatch,
     down: Stopwatch,
     left: Stopwatch,
-    right: Stopwatch,
+    right: Stopwatch
 }
 
 
 
 fn spawn_hitbox(mut commands: Commands,assets: Res<AssetServer>){
-    let player_render:Handle<Scene> = assets.load("start_character.glb#Scene0");
+    
+    let player_render = SceneBundle {
+        scene: assets.load("start_character.glb#Scene0"),
+        ..Default::default()
+    };
 
 
     commands.spawn(player_render)
     .insert(RigidBody::Dynamic)
+    .insert(AdditionalMassProperties::Mass(1.0))
     .insert(Player)
     .insert(PlayerHitbox)
-    .insert(Velocity::zero())
     .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)))
-    .insert(GravityScale(0.5))
-    .insert(LockedAxes::TRANSLATION_LOCKED | LockedAxes::ROTATION_LOCKED_X)
+    .insert(Velocity::zero())
+    .insert(Damping {linear_damping:0.0, angular_damping: 0.0})
+    .insert(GravityScale(1.0))
+    .insert(LockedAxes::ROTATION_LOCKED)
     .with_children(|children| {
         children.spawn(Collider::cylinder(0.5,0.5))
             // Position the collider relative to the rigid-body.
-            .insert(TransformBundle::from(Transform::from_xyz(0.0, 1.0, 0.0)));
+            .insert(TransformBundle::from(Transform::from_xyz(0.0, 1.0, 0.0)))
+            .insert(ActiveEvents::COLLISION_EVENTS);
         }
     );
 
@@ -75,6 +87,7 @@ fn spawn_time(mut commands: Commands){
 fn keyboard_walk(keys: Res<ButtonInput<KeyCode>>,mut movement_event_writer: EventWriter<MovementAction>,){
 
     let mut vel = Vec2::ZERO;
+
     //forward
     if keys.pressed(KeyCode::KeyW) {
         vel =  Vec2::new(0.0,1.0);
@@ -107,41 +120,67 @@ fn keyboard_dash(time: Res<Time>,keys: Res<ButtonInput<KeyCode>>,
     p_t.right.tick(Duration::from_secs_f32(time.delta_seconds()));
     
     let mut vel = Vec2::ZERO;
-
+    let mut dash = false;
+    
     if keys.just_released(KeyCode::KeyW){
         p_t.up.reset();
     }
     if p_t.up.elapsed_secs() <= 1.0 && keys.just_pressed(KeyCode::KeyW){
-        vel = Vec2::new(0.0,10.0);
+        vel = Vec2::new(0.0,1.0);
+        dash = true;
     }
     if keys.just_released(KeyCode::KeyS){
         p_t.down.reset();
     }
     if p_t.down.elapsed_secs() <= 1.0 && keys.just_pressed(KeyCode::KeyS){
-        vel = Vec2::new(0.0,-10.0);
+        vel = Vec2::new(0.0,-1.0);
+        dash = true;
     }
     if keys.just_released(KeyCode::KeyA){
         p_t.left.reset();
     }
     if p_t.left.elapsed_secs() <= 1.0 && keys.just_pressed(KeyCode::KeyA){
-        vel = Vec2::new(10.0,0.0);
+        vel = Vec2::new(1.0,0.0);
+        dash = true;
     }
     if keys.just_released(KeyCode::KeyD){
         p_t.right.reset();
     }
     if p_t.right.elapsed_secs() <= 1.0 && keys.just_pressed(KeyCode::KeyD){
-        vel = Vec2::new(-10.0,0.0);
+        vel = Vec2::new(-1.0,0.0);
+        dash = true;
     }
 
-    if vel != Vec2::ZERO{
-        movement_event_writer.send(MovementAction::Move(vel));
+    if dash == true{
+        movement_event_writer.send(MovementAction::Dash(vel));
     }
 
     }
+
+
+fn keyboard_jump(keys: Res<ButtonInput<KeyCode>>,mut movement_event_writer: EventWriter<MovementAction>,){
+        if keys.just_pressed(KeyCode::Space){
+            movement_event_writer.send(MovementAction::Jump);
+        }
+
+    }    
+
+fn display_events(
+    mut collision_events: EventReader<CollisionEvent>,
+    mut contact_force_events: EventReader<ContactForceEvent>,
+    ) {
+    for collision_event in collision_events.read() {
+        println!("Received collision event: {:?}", collision_event);
+    }
+
+    for contact_force_event in contact_force_events.read() {
+        println!("Received contact force event: {:?}", contact_force_event);
+    }
+}
 
 fn move_character(mut movement_event_reader: EventReader<MovementAction>,
     time: Res<Time>,
-    mut q_1: Query<&mut Velocity,With<PlayerHitbox>>,){
+    mut q_1: Query<&mut Velocity,With<Player>>,){
     for event in movement_event_reader.read() {
         for mut vel in &mut q_1 {
             match event {
@@ -149,10 +188,22 @@ fn move_character(mut movement_event_reader: EventReader<MovementAction>,
                     vel.linvel.x += direction.x * 30.0 * time.delta_seconds();
                     vel.linvel.z += direction.y * 30.0 * time.delta_seconds();
                 }
+                MovementAction::Dash(direction)=> {
+                    vel.linvel.x += direction.x *300.0 * time.delta_seconds();
+                    vel.linvel.z += direction.y * 300.0 * time.delta_seconds();
+                }
                 MovementAction::Jump =>{
-                    todo!()
+                    vel.linvel.y = 10.0   
                 }
             }
         }
+    }
+}
+
+fn apply_movement_damping(mut query: Query<&mut Damping,With<Player>>) {
+    for mut damping_factor in &mut query {
+        // Todo create state slowing down
+        // We could use `LinearDamping`, but we don't want to dampen movement along the Y axis
+        damping_factor.linear_damping = 0.9;
     }
 }
