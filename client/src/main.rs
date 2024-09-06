10375fe63ea4abd31a31989110be69f86a4b33b0
Sirmadeira <nicolaumori@gmail.com>
2024-09-06 17:33:34 -0300

    Lol you can import features

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,12 @@
 use client::create_app;
+use lightyear_examples_common::app::{Apps, Cli};
+use lightyear_examples_common::settings::read_settings;
+use lightyear_examples_common::settings::Settings;
 
 pub fn main() {
+    let cli = Cli::default();
+    let settings_str = include_str!("../assets/settings.ron");
+    let settings = read_settings::<Settings>(settings_str);
     let mut app = create_app();
     app.run();
 }
