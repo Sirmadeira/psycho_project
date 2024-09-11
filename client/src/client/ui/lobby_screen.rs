e8d406aa13c0743a3e2ccf12d0c6a6a9188abdfc
Sirmadeira <nicolaumori@gmail.com>
2024-09-11 18:09:19 -0300

    Getting started with lobby ui

---
+++ client/src/client/ui/lobby_screen.rs
@@ -0,0 +1,62 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct ScreenLobby;
+
+pub fn lobby_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let button_style = Style {
+        width: Val::Px(250.0),
+        height: Val::Px(75.0),
+        border: UiRect::all(Val::Px(15.0)),
+        margin: UiRect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+
+    let button_text_style = TextStyle {
+        font: asset_server.load("grafitti.ttf"),
+        font_size: 40.0,
+        color: Color::srgb(0.9, 0.9, 0.9),
+    };
+
+    // Root noded
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::SpaceBetween,
+                    ..default()
+                },
+                ..default()
+            },
+            ScreenLobby,
+        ))
+        // First column
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Lobby",
+                        TextStyle {
+                            font: asset_server.load("grafitti.ttf"),
+                            font_size: 80.0,
+                            color: Color::srgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+        });
+}
---
+++ client/src/client/ui/mod.rs
@@ -0,0 +1,26 @@
+use crate::client::MyAppState;
+use bevy::prelude::*;
+
+mod lobby_screen;
+mod main_screen;
+
+use self::{lobby_screen::*, main_screen::*};
+
+pub struct UiPlugin;
+
+impl Plugin for UiPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_begin_camera);
+        app.add_systems(OnEnter(MyAppState::MainMenu), menu_screen);
+        app.add_systems(OnEnter(MyAppState::Lobby), lobby_screen);
+        app.add_systems(Update, start_button.run_if(in_state(MyAppState::MainMenu)));
+        app.add_systems(Update, exit_button.run_if(in_state(MyAppState::MainMenu)));
+    }
+}
+
+#[derive(Component)]
+pub struct UiCamera;
+
+fn spawn_begin_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default()).insert(UiCamera);
+}
