43a0a9dc3ff6bf540df0e8a87cbf9853cfe2e90f
Sirmadeira <nicolaumori@gmail.com>
2024-09-15 10:38:07 -0300

    Getting started with modular character

--- client/src/client/load_assets.rs
+++ client/src/client/load_assets.rs
@@ -22,23 +22,11 @@ impl Plugin for LoadingAssetsPlugin {
 // Resource for easily acessing client based assets, which are mostly things like character world and so on. Each field in the connect is gonna be associate with something.
 #[derive(AssetCollection, Resource)]
 pub struct ClientCharCollection {
-    #[asset(paths("characters/character_mesh.glb"), collection(typed, mapped))]
+    #[asset(
+        paths("characters/character_mesh.glb", "character/katana.glb"),
+        collection(typed, mapped)
+    )]
     pub gltf_files: HashMap<String, Handle<Gltf>>,
-}
-
-#[derive(Resource, Reflect)]
-pub struct ConfigModularCharacters {
-    pub visual_to_attach: Vec<String>,
-    pub weapons_to_attache: Vec<String>,
-}
-
-impl Default for ConfigModularCharacters {
-    fn default() -> Self {
-        ConfigModularCharacters {
-            visual_to_attach: vec!["character_mesh".to_string()],
-            weapons_to_attache: vec!["katana".to_string()],
-        }
-    }
 }
 
 // This will spawn our main characters according TO THE AMOUNT OF ENTITIES, IN LOBBY. TODO LOBBY
--- client/src/shared/protocol/player_structs.rs
+++ client/src/shared/protocol/player_structs.rs
@@ -1,24 +1,42 @@
+use std::vec;
+
 use bevy::prelude::{Bundle, Component};
 use lightyear::prelude::*;
 use serde::{Deserialize, Serialize};
 
-// Player bundler - Create the matrix of our player it give me a serious of needed info
+// Player bundle - Shared player related info important to server and client
 #[derive(Bundle)]
 pub(crate) struct PlayerBundle {
     id: PlayerId,
+    visuals: PlayerVisuals,
 }
 
 impl PlayerBundle {
     pub(crate) fn new(id: ClientId) -> Self {
-        Self { id: PlayerId(id) }
+        Self {
+            id: PlayerId(id),
+            visuals: PlayerVisuals {
+                character: vec![String::from("character_mesh.glb")],
+                weapon: vec![String::from("Katana")],
+            },
+        }
     }
 }
 
 // Components
 
-// Marker component tells me exactly who connected, which player and so on. Via it is id also know as ip
+// Easy component that give me an easy way to acess the clientid of that specific player
 #[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct PlayerId(pub ClientId);
+
+// Since our character are modular we will be able to attach a series of visuals to it
+#[derive(Component, Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct PlayerVisuals {
+    // Character related visuals - Vec of file paths
+    character: Vec<String>,
+    // Character weapons - Vec of weapons
+    weapon: Vec<String>,
+}
 
 // Channels
 #[derive(Channel)]
